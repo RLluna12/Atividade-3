2b659014d3dad46c3f97995505f0fbc6c755e1b2
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-04 11:31:01 -0300

    exercicio 1

---
+++ Atividade 3, exercicio 1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+           
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+                for (int i = 0; i < 10; i++)
+            {
+                for (int j = i + 1; j <10; j++)
+                {
+                    if (j != i && a[j] == a[i])
+                    {
+                        Console.WriteLine("numeros repetidos  " + a[j]);
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
