b5601ccb4394cb8344ee1e8516a91cec4a404213
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-05 00:18:27 -0300

    2-numero negativo vira 0

---
+++ 2-numero negativo vira 0.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                
+                    if (a[i] <0)
+                    {
+                        a[i]=0;
+                    }
+                    Console.WriteLine(a[i]);
+                
+            }
+        }
+    }
+}
