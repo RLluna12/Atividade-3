4f382f3216f6baa5ba4dfcf1231f255d69886d32
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-07 23:40:23 -0300

    5- Metodo soma dos numeros impares do vetor

---
+++ 5- Metodo soma dos numeros impares do vetor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] v = new int[10];
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+
+            }
+            M(ref v);
+
+
+        }
+        public static void M(ref int[] x)
+        {
+            int a = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (x[i] % 2 == 1)
+                {
+                    a+=x[i];
+                }
+
+
+            }
+            Console.WriteLine("A soma dos numeros impar é: "+a);
+        }
+    }
+}
