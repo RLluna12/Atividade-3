a1c89f47efdccf9553af5296a4d6925f15d14aee
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-07 23:10:28 -0300

    3- celsius em fahrenheit- METODO

---
+++ 3- celsius em fahrenheit- METODO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            double c, f=0;
+            Console.WriteLine("temperatura em grau Celsius: ");
+            c = double.Parse(Console.ReadLine());
+            F(c, f);
+            
+        }
+        public static void F(double c, double f )
+        {
+
+            f = (9 * c + 160) / 5;
+            Console.WriteLine("a conversao de grau Celsius em grau Fahrenheit é : " + f);
+
+        }
+
+    }
+}
